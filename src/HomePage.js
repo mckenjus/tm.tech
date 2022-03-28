1cfd3396cb0b53029285cbf8fb190041722c85db
Justin McKenzie <mckenjus@gmail.com>
2022-03-28 09:46:26 -0600

    fixed styling

--- src/HomePage.js
+++ src/HomePage.js
@@ -6,7 +6,7 @@ function HomePage() {
   return(
     <div className="jumbotron">
         <h1>Welcome to My demo site!</h1>
-        <p>Please go to the "QB Comparison" page to get started!</p>
+        <p>Please go to the "QB Comparison" page, then select a player's name to see their stats!!</p>
     </div>
   );
 }
